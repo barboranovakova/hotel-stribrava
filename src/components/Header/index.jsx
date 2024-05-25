f7f32907c092a3be2cf750aff84b69fb2efd2dff
Barbora Nováková <barun.novak@gmail.com>
2024-05-25 11:37:23 +0200

    chyba s obrazkem

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,18 @@
+import './style.css';
+
+export const Header = () => {
+  return (
+    <header>
+      <div className="banner">
+        <div className="banner__stripe">
+          <div className="container">
+            <h1>Hotel Stříbrava</h1>
+            <p className="lead">
+              Rodinný hotel v malebné krajině českého středohoří.
+            </p>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
